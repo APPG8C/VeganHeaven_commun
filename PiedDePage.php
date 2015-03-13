f08acfcbfce51f5bc9e3ffad419711125dc4eb50
ced01 <cedlautard@gmail.fr>
2015-03-13 17:47:11 +0100

    Création graphique du pied de page

--- PiedDePage.php
+++ PiedDePage.php
@@ -1,5 +1,11 @@
-<footer class="pieddepage">
-<a href="AideEnligne.php">Aide en ligne</a>
-<a href="MentionsLégales.php">Mentions Légales</a>
-<a href="Contacts.php">Contacts</a>
+<footer class="footers">
+<nav>
+	<ul>
+		<li><a href="AideEnligne.php">Aide en ligne</a></li>
+		<li><a href="MentionsLégales.php">Mentions Légales</a></li>
+		<li><a href="Contacts.php">Contacts</a></li>
+</nav>
+	<p class="Copyright">
+	ALL RIGHT RESERVED | PHOTO BY PHOTOGRAPH
+	</p>
 </footer>
