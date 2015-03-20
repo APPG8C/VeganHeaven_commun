210be908a53a362a7ea71328ef85aa9e0ec32cc5
Ibrahim75 <ibra_lion_du75013@hotmail.fr>
2015-03-20 11:19:31 +0100

    Page de profil
    Page vierge

---
+++ PageDeProfil.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Votre profil</title>
+        <?php 
+        include("Entete.php");
+        setTitle($Accueil,true,$texte="Vegan Heaven !",$texte2="Besoin d'un bon avocat ?");
+        ?>
+        
+    </head>
+
+     <body>
+
+        <?php include("PiedDePage.php");?>
+    </body>
+    </html>
