57732ce7fd8f29e7ad350980f8eaf9c702a71fa1
qmatyas <qmatyas@gmail.com>
2015-03-25 10:34:36 +0100

    creation offres html

--- controleurs/RechercheControleurs.php
+++ controleurs/RechercheControleurs.php
@@ -1,5 +1,5 @@
 <?php
 include('./modeles/MenuModeles.php');
-setTitle($texte="Voici les offres",$texte2="Fruit-yez, Pauvre fous !"); 
+setTitle($texte="les offres",$texte2="Fruit-yez, Pauvre fous !"); 
 include('./vues/RechercheVues.php');
 ?>
