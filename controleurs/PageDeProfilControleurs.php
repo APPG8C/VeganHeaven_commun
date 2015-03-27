73b98df3c13c2f0bcb7d45624693db0cf006e4c3
Ibrahim75 <ibra_lion_du75013@hotmail.fr>
2015-03-27 15:16:45 +0100

    Page de profil
    Page de profil non terminé + Footers clear

---
+++ controleurs/PageDeProfilControleurs.php
@@ -0,0 +1,6 @@
+<?php
+include('/../modeles/MenuModeles.php');
+setTitle($texte="Vegan Heaven !",$texte2="Besoin d'un bon avocat ?");
+include('/../vues/PageDeProfilVues.php');
+
+?>
---
+++ vues/PageDeProfilVues.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Votre profil</title>
+    </head>
+
+    <body> 
+        <div id="header">
+            <h1 class="titles" id="votre">Votre page de profil</h1>
+            <img id="image" src="http://cdn.pocketfives.com/profiles/unknown-big.gif"/>
+        </div>
+        
+        <div class="left" class="dive">
+            <h2 class="titles">Vous</h2>
+            <img id="photoleft" src="http://cdn.pocketfives.com/profiles/unknown-big.gif"/>
+            <p></p>
+            <p></p>
+            <p></p>
+        </div>
+
+        <div class="right" class="dive">
+            <h2 class="titles">Modifier votre profil</h2>
+
+            <ul><></h3>
+                <li>Modifier vos informations personnelles</li>
+                <li></li>
+                <li></li>
+            </ul>
+
+        </div>
+        
+        <div class="center" class="dive">
+            <h2 class="titles">Vos informations</h2>
+            <ul>Informations générales
+                    <li>Nom Prénon</li>
+                    <li>Âge</li>
+                </ul>
+
+
+                <ul>Informations de contacts
+                    <li>Adresse</li>
+                    <li>Adresse mail</li>
+                    <li>Téléphone</li>
+                </ul>
+
+                <ul>Notes et commentaires des utilisateurs
+                    <li>Moyenne des notes: 5/5</li>
+                    <li>Avis : </li>
+            </ul>
+        </div>
+       <!--  <div id="header" class="profil">
+            <h1 class="title" id="titleprofil">Votre page de profil</h1>
+            <img id="image" src="http://cdn.pocketfives.com/profiles/unknown-big.gif"/>
+        </div>
+        
+        <div class="left" class="profil">
+            <h2 class="title">Vous</h2>
+            <img id="vous" src="http://cdn.pocketfives.com/profiles/unknown-big.gif"/>
+        </div>
+        
+        <div class="center" class="profil">
+            <h3 class="title">Vos informations</h3>
+            <ul>Informations générales
+                    <li>Nom Prénon</li>
+                    <li>Âge</li>
+                </ul>
+
+
+                <ul>Informations de contacts
+                    <li>Adresse</li>
+                    <li>Adresse mail</li>
+                    <li>Téléphone</li>
+                </ul>
+
+                <ul>Notes et commentaires des utilisateurs
+                    <li>Moyenne des notes: 5/5</li>
+                    <li>Avis : </li>
+            </ul>
+        </div> -->
+    </body>
+    </html>
