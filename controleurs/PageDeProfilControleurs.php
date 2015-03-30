b1d47e5720e5369a270a6d6e078116b62c66d77f
Ibrahim75 <ibra_lion_du75013@hotmail.fr>
2015-03-30 19:16:01 +0200

    Page de profil quasi fini
    Reste à changer les couleurs pck Cédric pleure

--- controleurs/PageDeProfilControleurs.php
+++ controleurs/PageDeProfilControleurs.php
@@ -1,6 +1,6 @@
 <?php
 include('/../modeles/MenuModeles.php');
-setTitle($texte="Vegan Heaven !",$texte2="Besoin d'un bon avocat ?");
+setTitle($texte="Vegan Heaven !",$texte2="Occuper vous de votre fraise !");
 include('/../vues/PageDeProfilVues.php');
 
 ?>
--- vues/PageDeProfilVues.php
+++ vues/PageDeProfilVues.php
@@ -1,31 +1,78 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Votre profil</title>
+        <title>Mon profil</title>
     </head>
 
     <body> 
         <div id="header">
-            <h1 class="titles" id="votre">Votre page de profil</h1>
-            <img id="image" src="http://cdn.pocketfives.com/profiles/unknown-big.gif"/>
+            <h1 class="titles" id="votre">Ma page de profil</h1>
+            <img id="image" src="images\contact.gif"/>
         </div>
         
         <div class="left" class="dive">
-            <h2 class="titles">Vous</h2>
-            <img id="photoleft" src="http://cdn.pocketfives.com/profiles/unknown-big.gif"/>
-            <p></p>
-            <p></p>
-            <p></p>
+            <h2 class="titles">Moi</h2>
+            <img id="photoleft" src="images\contact.gif"/>
+
+            <h4 style="text-align:center">Date d'inscription: 23/03/2015</h4>
+
+            <ol><h4>Mes fruits préférés</h4>
+                <li>Les pommes</li>
+                <li>Les oranges</li>
+                <li>Les figues</li>
+            </ol>
+
+            <ol><h4>Mes légumes préférés</h4>
+                <li>Les poivrons</li>
+                <li>Les tomates</li>
+                <li>Les herbes</li>
+            </ol>
         </div>
 
         <div class="right" class="dive">
-            <h2 class="titles">Modifier votre profil</h2>
+            <h2 class="titles">Gérer mon compte</h2>
 
-            <ul><></h3>
-                <li>Modifier vos informations personnelles</li>
-                <li></li>
-                <li></li>
+            <ul><h4>Modifier mes informations de profil</h4>
+                <a class="modification" href=""><li>Actualiser mes informations personnelles</li></a>
+                <a class="modification" href=""><li>Changer ma photo de profil</li></a>
+                <a class="modification" href=""><li>Changer mes fruits préférés</li></a>
             </ul>
+
+            <ul><h4>Gérer mes offres</h4>
+                <a class="modification" href=""><li>Ajouter une offre</li></a>
+                <a class="modification" href=""><li>Supprimer une offre</li></a>
+                <a class="modification" href=""><li>Voir mes offres</li></a>
+            </ul>
+
+            <table id="tablecontact">
+                <thead>
+                    <tr>
+                        <th colspan="3" style="text-align:center; text-decoration:underline">Mes contacts préférés</th>
+                    </tr>
+                </thaed>
+
+                <tbody>
+                    <tr>
+                        <td><a href=""><img class="imagecontact" src="images\contact.gif"></a></td>
+                        <td><a href=""><img class="imagecontact" src="images\contact.gif"></a></td>
+                        <td><a href=""><img class="imagecontact" src="images\contact.gif"></a></td>
+                    </tr>
+                    <tr>
+                        <td><a href=""><img class="imagecontact" src="images\contact.gif"></a></td>
+                        <td><a href=""><img class="imagecontact" src="images\contact.gif"></a></td>
+                        <td><a href=""><img class="imagecontact" src="images\contact.gif"></a></td>
+                    </tr>
+                    <tr>
+                        <td><a href=""><img class="imagecontact" src="images\contact.gif"></a></td>
+                        <td><a href=""><img class="imagecontact" src="images\contact.gif"></a></td>
+                        <td><a href=""><img class="imagecontact" src="images\contact.gif"></a></td>
+                    
+                    </tr>
+                    <tr>
+                        <td colspan="3" style="text-align:center; text-decoration:underline none; font-size:12px"><strong><a id="tablemodification" href="">Ajouter/supprimer des contacts préférés</a></strong></td>
+                    </tr>
+                </tbody>
+            </table>
 
         </div>
         
@@ -45,37 +92,23 @@
 
                 <ul>Notes et commentaires des utilisateurs
                     <li>Moyenne des notes: 5/5</li>
-                    <li>Avis : </li>
-            </ul>
-        </div>
-       <!--  <div id="header" class="profil">
-            <h1 class="title" id="titleprofil">Votre page de profil</h1>
-            <img id="image" src="http://cdn.pocketfives.com/profiles/unknown-big.gif"/>
-        </div>
-        
-        <div class="left" class="profil">
-            <h2 class="title">Vous</h2>
-            <img id="vous" src="http://cdn.pocketfives.com/profiles/unknown-big.gif"/>
-        </div>
-        
-        <div class="center" class="profil">
-            <h3 class="title">Vos informations</h3>
-            <ul>Informations générales
-                    <li>Nom Prénon</li>
-                    <li>Âge</li>
+                    <li>Avis : klm:"Plutôt sympa et une grande disponibilité pour les échanges"</li>
                 </ul>
 
-
-                <ul>Informations de contacts
-                    <li>Adresse</li>
-                    <li>Adresse mail</li>
-                    <li>Téléphone</li>
-                </ul>
-
-                <ul>Notes et commentaires des utilisateurs
-                    <li>Moyenne des notes: 5/5</li>
-                    <li>Avis : </li>
-            </ul>
-        </div> -->
+            <table id="maptable">
+                <thead>
+                    <tr>
+                        <th>Ma location</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                             <img id="mapphoto" src="images\map.PNG"/>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </body>
     </html>
